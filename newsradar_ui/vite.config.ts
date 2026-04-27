2b94ac3bcaa5c77d1404adffdf3333a58e970dd7
Eloy Martín Gámez <100495915@alumnos.uc3m.es>
2026-04-27 12:17:38 +0200

    fix(build): configurar base path para despliegue correcto en github pages

--- newsradar_ui/vite.config.ts
+++ newsradar_ui/vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/DevOps-gr84-A/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
